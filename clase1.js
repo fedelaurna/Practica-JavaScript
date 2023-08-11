795904b15ac828667722a3e26ac713bd90e7e3e3
fedelaurna <142011660+fedelaurna@users.noreply.github.com>
2023-08-11 08:35:59 -0300

    trabajo practico JavaScript

---
+++ clase1.js
@@ -0,0 +1,51 @@
+
+//actividaddes 1,2,3,4
+
+alert("bienvenido a mi pagina")
+let suma = 20 + 50
+let resta = 50-30
+let division = 40/10
+let multiplicacion = 10*15
+
+alert("mi edad es " + (2023-2009))
+alert("el resultado de la suma es " + suma )
+alert("el resultado de la resta es " + resta )
+alert("el resultado de la division es: " + division)
+alert("el resultado de la multiplicacion es: " + multiplicacion)
+
+
+console.log ("el resultado de la suma es: " + (50+20))
+console.log ("el resultado de la resta es: " + (50-30))
+console.log ("el resultado de la division es: " + (40/10))
+console.log ("el resultado de la multiplicacion es: " + (10*15))
+// actividad 5 
+let nacimiento
+
+nacimiento = prompt("ingrese su año de nacimiento")
+
+alert("tu edad es " +(2023 - nacimiento))
+
+ 
+// actividad 6
+let nombreAlumnoApellidoAlumno
+
+nombreAlumnoApellidoAlumno = prompt("ingrese tu nombre y apellido")
+
+alert("bienvenido " + nombreAlumnoApellidoAlumno)
+
+//actividad 7
+
+let nombrealumno = "fede "
+
+let apellidoAlumno = "laurna "
+
+console.log("biemvenido " +nombreAlumnoApellidoAlumno )
+
+  //actividad 8
+
+let diez = 10
+ let tres = 3
+ 
+ let resultadoMultiplicacion = tres * diez
+
+console.log(resultadoMultiplicacion)
